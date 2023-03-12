bfdb7b440d4842cedf693290f4b7616c407510f3
root <showell30@yahoo.com>
2023-03-12 07:38:24 +0000

    Improve some boolean-related stuff.

--- stepper.py
+++ stepper.py
@@ -3,6 +3,9 @@ class Expr:
         self.val = val
         self.label = label
 
+def VAL(x):
+    return Expr(val=x, label=x)
+
 def VAR(x, label):
     return Expr(val=x, label=label)
 
@@ -18,8 +21,10 @@ def MULT(x, y):
 def DECR(x):
     return Expr(val=x.val - 1, label=f"{x.label} - 1")
 
+ONE = Expr(val=1, label="1")
+
 def NOT(x):
-    return MULT(DECR(x), DECR(x))
+    return SUB(ONE, x)
 
 def AND(x, y):
     return MULT(x, y)
@@ -27,7 +32,7 @@ def AND(x, y):
 def OR(x, y):
     return SUB(ADD(x,y), MULT(x,y))
 
-def ORR(x, y, z):
+def OR3(x, y, z):
     return OR(OR(x, y), z)
 
 def evaluate(*, hb, lb, halted, accepted, op_hb, op_lb):
@@ -50,12 +55,12 @@ def evaluate(*, hb, lb, halted, accepted, op_hb, op_lb):
     
     becomes_3 = AND(is_3, stays_same)
     becomes_2 = OR(AND(is_2, stays_same), AND(is_3, runs_decr))
-    becomes_1 = ORR(AND(is_1, stays_same), AND(is_2, runs_decr), AND(is_odd, runs_mod2))
-    becomes_0 = ORR(AND(is_0, stays_same), AND(is_1, runs_decr), AND(is_even, runs_mod2))
+    becomes_1 = OR3(AND(is_1, stays_same), AND(is_2, runs_decr), AND(is_odd, runs_mod2))
+    becomes_0 = OR3(AND(is_0, stays_same), AND(is_1, runs_decr), AND(is_even, runs_mod2))
 
     accepted = OR(accepted, AND(is_0, runs_check))
 
-    halted = ORR(halted, accepted, AND(is_0, runs_decr))
+    halted = OR3(halted, accepted, AND(is_0, runs_decr))
 
     hb_set = OR(becomes_3, becomes_2)
     lb_set = OR(becomes_3, becomes_1)
@@ -73,10 +78,11 @@ def step(*, AX, halted, accepted, op):
     op_hb = op // 2
     op_lb = op % 2
 
-    hb = VAR(hb, "a")
-    lb = VAR(lb, "b")
-    halted = VAR(halted, "c")
-    accepted = VAR(accepted, "d")
+    hb = VAL(hb)
+    lb = VAL(lb)
+
+    halted = VAR(halted, "halted")
+    accepted = VAR(accepted, "accepted")
     op_hb = VAR(op_hb, "ophb1")
     op_lb = VAR(op_lb, "ophb2") 
 
