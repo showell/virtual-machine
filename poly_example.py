02cb17b2b93295825f2b6242dce4e39e1b0a89c3
root <showell30@yahoo.com>
2023-03-15 17:38:28 +0000

    Improve partial examples.

--- poly_example.py
+++ poly_example.py
@@ -82,15 +82,24 @@ assert a + b == b + a
 assert a * b == b * a
 assert (a + 3) ** 2 == a**2 + 6 * a + 9
 
-# You can do partial application of variables on polynomials
+# You can do partial application of variable assignments on polynomials.
 assert (x + y**2).apply(y=2) == x + 4
-assert (x + y**2 + z).apply(y=3) == x + z + 9
+assert (x + y**2 + z).apply(y=3) == x + 9 + z
+assert (x + y + z + 4).apply(x=1000, y=200, z=30) == Poly.constant(1234)
 assert (x + y + z + 4).apply(x=1000, y=200, z=30).eval() == 1234
+assert (x + y + z + 4).apply(x=1000, y=200) == z + 1204
 assert (x + y + z + 4).apply(x=1000, y=200).eval(z=30) == 1234
 
 # Partial application always returns a new polynomial.
 p = (x + 43) * (y - 37) * (z + 2)
+assert_equal(p.variables(), {"x", "y", "z"})
 assert type(p.apply(x=5, y=2, z=44)) == Poly
 assert type(p.apply(x=9, y=8)) == Poly
-assert type(p.apply(x=5, z=44)) == Poly
+assert type(p.apply(x=5, z=22)) == Poly
+
+# Each partial application of a variable results in a polynomial with
+# a subset of variables from the original one.
+assert_equal(p.apply(x=5, z=22).variables(), {"y"})
+
+# We allow silly calls to apply that do nothing.
 assert p.apply() == p
